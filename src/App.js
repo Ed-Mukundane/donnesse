607f942a2a0a06bdf4857516aa5a30e6e73a19f2
Ed-Mukundane <aedgar990@gmail.com>
2021-10-18 13:27:27 +0300

    donnesse app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter as Router, Link } from "react-router-dom";
+import About from "./pages/AboutPage";
+import Home from "./pages/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<Router>
+			<div>
+				<h3>Hello World!</h3>
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/about">About</Link>
+						</li>
+					</ul>
+				</nav>
+
+				<Route path="/" exact component={Home} />
+				<Route path="/about" component={About} />
+			</div>
+		</Router>
+	);
 }
 
 export default App;
